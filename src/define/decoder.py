08e885acfa58aebe273a5062b7f692c300c8c908
adityajain <2003adityajain@gmail.com>
2020-07-19 17:36:01 -0400

    stopped re-trimming for l=2

--- src/define/decoder.py
+++ src/define/decoder.py
@@ -6,6 +6,7 @@ from define.chanreps import PauliConvertToTransfer
 from define.qcode import ComputeAdaptiveDecoder
 from define import fnames as fn
 
+
 def GetLeadingPaulis(lead_frac, qcode, chan_probs):
     # Get the leading Pauli probabilities in the iid model.
     # To get the indices of the k-largest elements: https://stackoverflow.com/questions/6910641/how-do-i-get-indices-of-n-maximum-values-in-a-numpy-array
@@ -13,22 +14,38 @@ def GetLeadingPaulis(lead_frac, qcode, chan_probs):
     if chan_probs.ndim == 1:
         iid_chan_probs = chan_probs
     else:
-        iid_chan_probs = ut.GetErrorProbabilities(qcode.PauliOperatorsLST, chan_probs, 0)
+        iid_chan_probs = ut.GetErrorProbabilities(
+            qcode.PauliOperatorsLST, chan_probs, 0
+        )
     leading_paulis = np.argsort(iid_chan_probs)[-nPaulis:]
     return (1 - iid_chan_probs[0], leading_paulis, iid_chan_probs[leading_paulis])
 
-def CompleteDecoderKnowledge(leading_fraction, chan_probs, qcode, level):
+
+def CompleteDecoderKnowledge(leading_fraction, chan_probs, qcode):
     # Complete the probabilities given to a ML decoder.
-    (infid, known_paulis, known_probs) = GetLeadingPaulis(leading_fraction, qcode, chan_probs)
+    (infid, known_paulis, known_probs) = GetLeadingPaulis(
+        leading_fraction, qcode, chan_probs
+    )
+    print(
+        "Number of known paulis in decoder knowledge = {}".format(known_paulis.shape[0])
+    )
+    print("Total known probability = {}".format(np.sum(known_probs)))
     decoder_probs = CreateIIDPauli(infid, qcode)
     decoder_probs[known_paulis] = known_probs
     total_unknown = 1 - np.sum(known_probs)
+    print("Total unknown probability = {}".format(total_unknown))
     # Normalize the unknown Paulis
     # https://stackoverflow.com/questions/27824075/accessing-numpy-array-elements-not-in-a-given-index-list
-    mask = np.ones(decoder_probs.shape[0], dtype = bool)
+    mask = np.ones(decoder_probs.shape[0], dtype=bool)
     mask[known_paulis] = False
-    decoder_probs[mask] = total_unknown * decoder_probs[mask]/np.sum(decoder_probs[mask])
+    decoder_probs[mask] = (
+        total_unknown * decoder_probs[mask] / np.sum(decoder_probs[mask])
+    )
+    print("Sum of decoder probs = {}".format(np.sum(decoder_probs)))
+    print("Sorted decoder probs = {}".format(np.sort(decoder_probs)))
+    exit
     return decoder_probs
+
 
 def PrepareChannelDecoder(submit, noise, sample):
     # Generate the decoder input for a given channel in the submission.
@@ -36,20 +53,39 @@ def PrepareChannelDecoder(submit, noise, sample):
     for l in range(submit.levels):
         if l == 0:
             qcode = submit.eccs[l]
-            (nrows, ncols) = (4**submit.eccs[l].K, 4**submit.eccs[l].K)
-            diagmask = [q * ncols * nrows + ncols * j + j for q in range(qcode.N) for j in range(nrows)]
+            (nrows, ncols) = (4 ** submit.eccs[l].K, 4 ** submit.eccs[l].K)
+            diagmask = [
+                q * ncols * nrows + ncols * j + j
+                for q in range(qcode.N)
+                for j in range(nrows)
+            ]
             rawchan = np.load(fn.RawPhysicalChannel(submit, noise))[sample, :]
             if submit.iscorr == 0:
-                chan_probs = np.reshape(np.tile(rawchan, qcode.N)[diagmask], [qcode.N, nrows])
+                chan_probs = np.reshape(
+                    np.tile(rawchan, qcode.N)[diagmask], [qcode.N, nrows]
+                )
             elif submit.iscorr == 2:
                 chan_probs = np.reshape(rawchan[diagmask], [qcode.N, nrows])
             else:
                 chan_probs = rawchan
+            decoder_probs = CompleteDecoderKnowledge(
+                submit.decoder_fraction, chan_probs, qcode
+            )
         elif l == 1:
             ComputeAdaptiveDecoder(qcode, decoder_probs)
-            chan_probs = np.tile([ComputeResiduals(p, decoder_probs, submit.eccs[0], lookup=qcode.tailored_lookup) for p in range(4)], [submit.eccs[1].N, 1])
+            chan_probs = np.tile(
+                [
+                    ComputeResiduals(
+                        p, decoder_probs, submit.eccs[0], lookup=qcode.tailored_lookup
+                    )
+                    for p in range(4)
+                ],
+                [submit.eccs[1].N, 1],
+            )
+            decoder_probs = ut.GetErrorProbabilities(
+                qcode.PauliOperatorsLST, chan_probs, 0
+            )
         else:
             pass
-        decoder_probs = CompleteDecoderKnowledge(submit.decoder_fraction, chan_probs, qcode, l)
         decoder_knowledge[l] = PauliConvertToTransfer(decoder_probs, qcode)
     return decoder_knowledge
