1526ea65c6dcc2f3edd58e3d3f9e1ad0f112a6ff
Pavithran Iyer <pavithran.iyer@uwaterloo.ca>
2020-09-15 11:29:36 -0400

    stable soft decoder

--- run_partial.sh
+++ run_partial.sh
@@ -6,10 +6,16 @@ if [[ $host == *"paviws"* ]]; then
 	chflowdir="/Users/pavi/Dropbox/rclearn"
 	cores=$(sysctl -n hw.ncpu)
 	sed_prepend="'' "
+	nonpauli_timestamps=("21_07_2020_16_28_27" "20_07_2020_20_11_41" "21_07_2020_21_49_52" "03_08_2020_22_47_51" "03_08_2020_23_03_35" "03_08_2020_22_54_43" "04_08_2020_18_31_52" "04_08_2020_18_49_45" "04_08_2020_19_00_34" "21_07_2020_16_28_30" "25_08_2020_01_24_08" "25_08_2020_01_24_23")
+	pauli_timestamps=("10_08_2020_00_33_27" "10_08_2020_00_33_34" "10_08_2020_00_33_35" "10_08_2020_00_33_36" "10_08_2020_10_04_35" "10_08_2020_10_04_37" "10_08_2020_16_53_00" "10_08_2020_16_53_01" "10_08_2020_16_53_02" "10_08_2020_16_53_03" "10_08_2020_16_53_04" "10_08_2020_16_53_05")
+	alphas=(0 0.0001 0.0003 0.0005 0.0007 0.0012 0.0025 0.0036 0.005 0.01 0.02 1)
 elif [[ $host == "oem-ThinkPad-X1-Carbon-Gen-8" ]]; then
 	outdir="/home/oem/Documents"
 	chflowdir="/home/oem/Desktop/Research_PhD"
 	cores=$(nproc --all)
+	nonpauli_timestamps=("21_07_2020_16_28_27" "03_08_2020_22_54_43" "04_08_2020_18_31_52" "04_08_2020_18_49_45" "04_08_2020_19_00_34" "21_07_2020_16_28_30" "25_08_2020_01_24_08" "25_08_2020_01_24_23")
+	pauli_timestamps=("10_08_2020_00_33_27" "10_08_2020_10_04_37" "10_08_2020_16_53_00" "10_08_2020_16_53_01" "10_08_2020_16_53_02" "10_08_2020_16_53_03" "10_08_2020_16_53_04" "10_08_2020_16_53_05")
+	alphas=(0 0.0001 0.0003 0.0007 0.0012 0.005 0.02 1)
 else
 	outdir="/project/def-jemerson/chbank"
 	chflowdir="/project/def-jemerson/pavi/chflow"
@@ -30,20 +36,7 @@ display() {
 	./chflow.sh $ts
 }
 
-pauli_timestamps=("21_07_2020_16_28_27" "20_07_2020_20_11_41" "21_07_2020_21_49_52" "03_08_2020_22_47_51" "03_08_2020_23_03_35" "03_08_2020_22_54_43" "04_08_2020_18_31_52" "04_08_2020_18_49_45" "04_08_2020_19_00_34" "21_07_2020_16_28_30" "25_08_2020_01_24_08" "25_08_2020_01_24_23")
-
-cptp_timestamps=("06_08_2020_19_31_31" "06_08_2020_19_31_32" "06_08_2020_19_31_34" "06_08_2020_19_31_35" "06_08_2020_19_31_37" "06_08_2020_19_31_49" "06_08_2020_19_31_50" "06_08_2020_19_31_52" "06_08_2020_19_31_53" "06_08_2020_19_31_55")
-
-rtasu_timestamps=("07_08_2020_00_44_26" "07_08_2020_00_44_27" "07_08_2020_00_44_29" "07_08_2020_00_44_30" "07_08_2020_00_44_31" "07_08_2020_00_44_33" "07_08_2020_00_44_36" "07_08_2020_00_44_38" "07_08_2020_00_44_39" "07_08_2020_00_44_40")
-
-#nonpauli_timestamps=("10_08_2020_00_33_27" "10_08_2020_00_33_29" "10_08_2020_00_33_30" "10_08_2020_00_33_31" "10_08_2020_00_33_33" "10_08_2020_00_33_34" "10_08_2020_00_33_35" "10_08_2020_00_33_36" "10_08_2020_10_04_35" "10_08_2020_10_04_37" "10_08_2020_16_53_00" "10_08_2020_16_53_01" "10_08_2020_16_53_02" "10_08_2020_16_53_03" "10_08_2020_16_53_04" "10_08_2020_16_53_05" "10_08_2020_16_53_06" "10_08_2020_16_53_07" "10_08_2020_16_53_08" "10_08_2020_10_04_38" "10_08_2020_10_04_39")
-nonpauli_timestamps=("10_08_2020_00_33_27" "10_08_2020_00_33_34" "10_08_2020_00_33_35" "10_08_2020_00_33_36" "10_08_2020_10_04_35" "10_08_2020_10_04_37" "10_08_2020_16_53_00" "10_08_2020_16_53_01" "10_08_2020_16_53_02" "10_08_2020_16_53_03" "10_08_2020_16_53_04" "10_08_2020_16_53_05")
-
-timestamps=("${nonpauli_timestamps[@]}")
-
-#alphas=(0 0.0001 0.0002 0.0003 0.0004 0.0007 0.0012 0.0019 0.0031 0.005 0.0085 0.0144 0.0245 0.0416 0.0707 0.1201 0.204 0.3466 0.5887 1 "ML")
-alphas=(0 0.0001 0.0003 0.0004 0.0007 0.0012 0.0025 0.0036 0.005 0.0144 0.0416 1)
-
+timestamps=("${pauli_timestamps[@]}")
 
 if [[ "$1" == "overwrite" ]]; then
 	rm input/partial_decoders.txt
@@ -84,15 +77,15 @@ elif [[ "$1" == "generate" ]]; then
 		./chflow.sh -- temp.txt
 		rm input/temp.txt
 
-		if [[ "$alpha" == "ML" ]]; then
-			echo "REPLACE decoder 1,1 WITH decoder 0,0 IN input/${ts}.txt"
-			sed -i ${sed_prepend}"s/decoder 1,1/decoder 0,0/g" input/${ts}.txt
-		else
-			echo "REPLACE decoder 1,1 WITH decoder 3,3 IN input/${ts}.txt"
-			sed -i ${sed_prepend}"s/decoder 1,1/decoder 3,3/g" input/${ts}.txt
-			echo "REPLACE dcfraction ${refalpha} WITH dcfraction ${alpha} IN input/${ts}.txt"
-			sed -i ${sed_prepend}"s/dcfraction ${refalpha}/dcfraction ${alpha}/g" input/${ts}.txt
-		fi
+		# echo "REPLACE decoder 1,1 WITH decoder 3,3 IN input/${ts}.txt"
+		# sed -i ${sed_prepend}"s/decoder 1,1/decoder 3,3/g" input/${ts}.txt
+		# echo "REPLACE dcfraction ${refalpha} WITH dcfraction ${alpha} IN input/${ts}.txt"
+		# sed -i ${sed_prepend}"s/dcfraction ${refalpha}/dcfraction ${alpha}/g" input/${ts}.txt
+
+		echo "REPLACE decoder 1,1,1 WITH decoder 3,3,3 IN input/${ts}.txt"
+		sed -i ${sed_prepend}"s/decoder 1,1,1/decoder 3,3,3/g" input/${ts}.txt
+		echo "REPLACE dcfraction ${refalpha} WITH dcfraction ${alpha} IN input/${ts}.txt"
+		sed -i ${sed_prepend}"s/dcfraction ${refalpha}/dcfraction ${alpha}/g" input/${ts}.txt
 
 		# echo "REPLACE ecc Steane WITH ecc Steane,Steane IN input/${ts}.txt"
 		# sed -i ${sed_prepend}"s/ecc Steane/ecc Steane,Steane/g" input/${ts}.txt
