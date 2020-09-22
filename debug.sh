ee32a4e537f6e32c1f4a554301842159459aa5f9
aditya2003 <2003adityajain@gmail.com>
2020-09-22 11:33:18 +0530

    deleted everything

--- debug.sh
+++
@@ -1,3 +0,0 @@
-rm /Users/pavi/Documents/chbank/19_08_2020_00_56_48/channels/*
-rm /Users/pavi/Documents/chbank/19_08_2020_00_56_48/metrics/*
-./chflow.sh 19_08_2020_00_56_48
--- input/cedar/partial_decoders.sh
+++
@@ -1,3 +0,0 @@
-module load intel python scipy-stack
-cd /project/def-jemerson/pavi/chflow
-parallel --joblog partial_decoders.log --jobs  ./chflow.sh {1} :::: input/partial_decoders.txt
