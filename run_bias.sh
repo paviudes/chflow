8b8e846a39c04024f087ec902fd4e4c0c9117129
aditya2003 <2003adityajain@gmail.com>
2021-03-20 01:58:11 +0530

    chflow aditya

--- run_bias.sh
+++ run_bias.sh
@@ -1,3 +1,6 @@
+./chflow.sh -- pauli.txt
+sed -i 's/Steane/7qc_cyclic/g' input/vary_bias_cycy.txt
 ./chflow.sh vary_bias_stst
-./chflow.sh vary_bias_fqfq
 ./chflow.sh vary_bias_cycy
+./chflow.sh -- test.txt
+./run_partial.sh compare_plot
