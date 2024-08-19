2054eae9a6ef848e270977128bbe3db6c561ebc3
paviudes <pavithran.sridhar@gmail.com>
2024-08-18 21:39:11 -0400

    parallel ptm

--- runs.sh
+++ runs.sh
@@ -1,9 +1,7 @@
-#./chflow.sh cptp_split
-#./chflow.sh cptp_dp
+./chflow.sh unitary_split
+./chflow.sh unitary_split_0.01
 ./chflow.sh -- gather.txt 1
 echo "DC 0"
-cat ./../chbank/cptp/regen/cptp_dp/results/log_infid.txt
+cat ./../chbank/unitary/regen/unitary_split/results/log_infid.txt
 echo "DC 0.01"
-cat ./../chbank/cptp/regen/cptp_split/results/log_infid.txt
-echo "DC 0.1"
-cat ./../chbank/cptp/regen/cptp_split_0.1/results/log_infid.txt
+cat ./../chbank/unitary/regen/unitary_split_0.01/results/log_infid.txt
